62da42c781f250d516fa0a20a5ab55bd577afb17
IMAN <iman.dolatkia@gmail.com>
2021-10-13 18:48:23 +0330

    change statuse bar color

--- horizontallyCardsLibrary/src/main/java/com/dolatkia/horizontallycardslibrary/PresentationUtils.kt
+++ horizontallyCardsLibrary/src/main/java/com/dolatkia/horizontallycardslibrary/PresentationUtils.kt
@@ -1,8 +1,13 @@
 package com.dolatkia.horizontallycardslibrary
 
+import android.app.Activity
 import android.content.Context
+import android.graphics.Color
 import android.os.Build
 import android.util.DisplayMetrics
+import android.view.View
+import android.view.WindowManager
+import androidx.core.content.ContextCompat
 import kotlin.math.roundToInt
 
 class PresentationUtils {
@@ -32,6 +37,35 @@ class PresentationUtils {
         fun convertDpToPixel(dp: Int, context: Context): Int {
             val px = dp * (getDisplayMetrics(context)!!.densityDpi / 160f)
             return px.roundToInt()
+        }
+
+        fun setStatusBarBackgroundColor(activity: Activity, color: Int) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                val window = activity.window
+                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+                window.statusBarColor = color
+                val decorView = window.decorView
+                var flags = decorView.systemUiVisibility
+                if (PresentationUtils.isColorLight(color)) {
+                    if (flags and View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR == 0) {
+                        flags = flags or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+                        decorView.systemUiVisibility = flags
+                    }
+                } else {
+                    if (flags and View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR != 0) {
+                        flags = flags and View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR.inv()
+                        decorView.systemUiVisibility = flags
+                    }
+                }
+            }
+        }
+
+        private fun isColorLight(color: Int): Boolean {
+            val darkness =
+                1 - (0.299 * Color.red(color) + 0.587 * Color.green(color) + 0.114 * Color.blue(
+                    color
+                )) / 255
+            return darkness < 0.5
         }
     }
 }
--- sample/src/main/java/com/dolatkia/horizontallycards/MainActivity.kt
+++ sample/src/main/java/com/dolatkia/horizontallycards/MainActivity.kt
@@ -5,11 +5,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
 import com.dolatkia.horizontallycards.databinding.ActivityMainBinding
+import com.dolatkia.horizontallycardslibrary.PresentationUtils
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+    lateinit var adapter: MyFullScreenCardViewPagerAdapter
     private val isRtl = false
+    private lateinit var myViewPagerListener: MyViewPagerListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +27,21 @@ class MainActivity : AppCompatActivity() {
         // set customize adapter to fullScreenCardViewPager
         // 0 = start position
         // MyFullScreenCardViewPagerAdapter = my customize adapter
-        binding.fullScreenCardViewPager.setAdapter(MyFullScreenCardViewPagerAdapter(this), 0)
+        adapter = MyFullScreenCardViewPagerAdapter(this)
+        binding.fullScreenCardViewPager.setAdapter(adapter, 0)
+
+        //create listener
+        myViewPagerListener = MyViewPagerListener(this)
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        adapter.addListener(myViewPagerListener)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        adapter.removeListener(myViewPagerListener)
     }
 }
